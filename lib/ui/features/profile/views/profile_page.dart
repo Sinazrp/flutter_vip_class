c0f1faa6ca5c95f04588d92e9a2a42547c283237
Iliya <iliyayekta384@gmail.com>
2026-06-15 22:36:06 +0330

    for test

--- lib/ui/features/profile/views/profile_page.dart
+++ lib/ui/features/profile/views/profile_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_vip_class/ui/core/validators/form_validators.dart';
 import 'package:flutter_vip_class/ui/features/profile/view_models/profile_view_model.dart';
-import 'package:flutter_vip_class/ui/features/simplepage.dart';
 import '../../../core/theme/app_text_styles.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -21,101 +20,81 @@ class _ProfilePageState extends State<ProfilePage> {
       appBar: AppBar(title: const Text('Profile Setup')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const Text(
-                  'Enter your profile information first.',
-                  style: AppTextStyles.headlineMedium,
-                ),
-                const SizedBox(height: 24),
-
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Name',
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: FormValidators.validateName,
-                  onSaved: (value) {
-                    _viewModel.updateName(value ?? '');
-                  },
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Age',
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.number,
-                  validator: FormValidators.validateAge,
-
-                  onSaved: (value) {
-                    _viewModel.updateAge(value ?? '');
-                  },
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: FormValidators.validateEmail,
-                  onSaved: (value) {
-                    _viewModel.updateEmail(value ?? '');
-                  },
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Phone Number',
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.phone,
-                  validator: FormValidators.validatePhoneNumber,
-                  onSaved: (value) {
-                    _viewModel.updatePhoneNumber(value ?? '');
-                  },
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                  obscureText: true,
-                  validator: FormValidators.validatePassword,
-                  onSaved: (value) {
-                    _viewModel.updatePassword(value ?? '');
-                  },
-                ),
-                const SizedBox(height: 24),
-                //CustomButton(submitProfile: submitProfile),
-                ElevatedButton(
-                  onPressed: () {
-                    _viewModel.submitProfile(context, _formKey);
-                  },
-                  child: const Text('Continue to Tasks'),
-                ),
-                const SizedBox(height: 20),
-                // ElevatedButton(
-                //   onPressed: () {
-                //     Navigator.push(
-                //       context,
-                //       MaterialPageRoute(
-                //         builder: (context) => const Simplepage(),
-                //       ),
-                //     );
-                //   },
-
-                //   child: const Text('go to simple page'),
-                // ),
-              ],
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          // addAutomaticKeepAlives: true,
+          children: [
+            Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              margin: EdgeInsets.all(10),
+              child: Center(child: Text("data")),
             ),
-          ),
+            Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              margin: EdgeInsets.all(10),
+              child: Center(child: Text("data")),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              margin: EdgeInsets.all(10),
+              child: Center(child: Text("data")),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              margin: EdgeInsets.all(10),
+              child: Center(child: Text("data")),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              margin: EdgeInsets.all(10),
+              child: Center(child: Text("data")),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              margin: EdgeInsets.all(10),
+              child: Center(child: Text("data")),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              margin: EdgeInsets.all(10),
+              child: Center(child: Text("data")),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              margin: EdgeInsets.all(10),
+              child: Center(child: Text("data")),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              margin: EdgeInsets.all(10),
+              child: Center(child: Text("data")),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              margin: EdgeInsets.all(10),
+              child: Center(child: Text("data")),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              margin: EdgeInsets.all(10),
+              child: Center(child: Text("data")),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              margin: EdgeInsets.all(10),
+              child: Center(child: Text("data")),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              margin: EdgeInsets.all(10),
+              child: Center(child: Text("data")),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              margin: EdgeInsets.all(10),
+              child: Center(child: Text("data 10")),
+            ),
+          ],
         ),
       ),
     );
